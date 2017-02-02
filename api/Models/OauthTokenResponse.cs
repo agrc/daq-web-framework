fc215baa606797161ba7485d7eabc73e05f05940
steveoh <sgourley@utah.gov>
2017-02-02 11:51:26 -0700

    update ago client to request oauth token

---
+++ api/Models/OauthTokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace daq_api.Models
+{
+    public class OauthTokenResponse : Errorable
+    {
+        public string Access_Token { get; set; }
+        public int Expires_In { get; set; }
+    }
+}
