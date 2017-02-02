8f29413fa690eccd2a75222cd321e26fb15c9339
steveoh <sgourley@utah.gov>
2017-02-02 11:51:47 -0700

    update api to send token to js app

--- api/Modules/HomeModule.cs
+++ api/Modules/HomeModule.cs
@@ -1,12 +1,18 @@
-﻿using Nancy;
+﻿using daq_api.Services;
+using Nancy;
 
 namespace daq_api.Modules
 {
     public class HomeModule : NancyModule
     {
-        public HomeModule()
+        public HomeModule(ArcOnlineHttpClient client)
         {
-            Get["/"] = _ => View["index"];
+            Get["/", true] = async (_, ctx) =>
+            {
+                var token = await client.GetToken();
+
+                return View["index", token];
+            };
         }
     }
 }
