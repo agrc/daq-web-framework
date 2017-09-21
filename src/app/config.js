bc41049b9db3eca074427de943ad39fcf8e7f5ae
steveoh <sgourley@utah.gov>
2017-09-21 09:43:25 -0600

    Release v1.10.2

--- src/app/config.js
+++ src/app/config.js
@@ -16,7 +16,7 @@ define([
     var config = {
         appName: 'daq-web',
         appId: 'Rngn0PfKFjvT2p26',
-        version: '1.10.1',
+        version: '1.10.2',
         maxResult: 1000,
         fields: {
             uniqueId: 'OBJECTID',
