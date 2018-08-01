552e4567424d2e6e8517af67af66d5dfa83ef29b
steveoh <sgourley@utah.gov>
2018-07-31 18:12:35 -0600

    format bytes to readable strings

---
+++ api/Services/ReadableFileSizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace daq_api.Services
+{
+    public static class ReadableFileSizer
+    {
+        private static readonly string[] SizeAbbreviations = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
+
+        public static string MakeReadable(long byteCount)
+        {
+            if (byteCount == 0)
+            {
+                return "0";
+            }
+
+            var bytes = Math.Abs(byteCount);
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+
+            return Math.Sign(byteCount) * num + SizeAbbreviations[place];
+        }
+    }
+}
